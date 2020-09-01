9ea0a1a2002dc385830f288ae56e07be93af5a29
Leobaldo Alcantara Neto <leobaldoneto@gmail.com>
2020-08-31 23:33:01 -0300

    Primeiro commit. Adicionei pedidos.

---
+++ src/bling.js
@@ -0,0 +1,40 @@
+'use strict';
+const axios = require('axios');
+
+const Pedidos = require('./pedidos');
+
+class Bling {
+  constructor(...args) {
+    this.axiosInstance = new axios.create({
+      baseURL: 'https://bling.com.br/Api/v2/',
+      timeout: 5000,
+      params: {
+        apikey: args[0].apikey,
+      },
+    });
+
+    this.pedidos = new Pedidos(this);
+  }
+
+  request(options) {
+    return new Promise((resolve, reject) => {
+      this.axiosInstance
+        .request(options)
+        .then(function (response) {
+          let retorno = response.data.retorno;
+          if (retorno.erros) {
+            reject(new Error(retorno.erros[0].erro.msg));
+          }
+          resolve(retorno);
+        })
+        .catch(function (error) {
+          reject(new Error(error.message));
+        });
+    });
+  }
+}
+
+module.exports = Bling;
+module.exports.config = (config) => {
+  return new Bling(config);
+};
---
+++ src/pedidos.js
@@ -0,0 +1,74 @@
+'use strict';
+const xmlbuilder = require('xmlbuilder2');
+
+module.exports = class Pedidos {
+  constructor(client) {
+    this.bling = client;
+  }
+
+  // Recebe um objeto pedido e retorna o pedido cadastrado.
+  async add(orderObject, notafiscal = false) {
+    let orderXML = xmlbuilder.create(order).toString();
+    let params = {
+      xml: orderXML,
+    };
+
+    if (notafiscal === true) {
+      params.gerarnfe = 'true';
+    }
+
+    let config = {
+      method: 'post',
+      url: 'pedido/json/',
+      params,
+    };
+    return this.bling.request(config);
+  }
+
+  // Atualizar a situação de um pedido
+  async update(orderNumber, orderStateObject) {
+    let orderXML = xmlbuilder.create(orderStateObject).toString();
+    let params = {
+      xml: orderXML,
+    };
+    let config = {
+      method: 'put',
+      url: `pedido/${orderNumber}/json`,
+      params,
+    };
+    return this.bling.request(config);
+  }
+
+  /* Retorna todos os produtos, no máximo 100 por página.
+    Para implementar:
+      - adicionar parametro para limitar o número de pedidos que deve ser retornado.
+      - padronizar os filtros em objetos
+  */
+  async getAll(filters = '') {
+    let params = {};
+
+    if (filters !== '') {
+      params.filters = filters;
+    }
+
+    let config = {
+      method: 'get',
+      url: 'pedidos/json/',
+      params,
+    };
+
+    return this.bling.request(config);
+  }
+
+  /*  Retorna pedido através do id do pedido.
+      Implementar:
+      - opção para usar o parametro de histórico do pedido da API do Bling.
+  */
+  async getByNumber(orderNumber) {
+    let config = {
+      method: 'get',
+      url: `pedido/${orderNumber}/json/`,
+    };
+    return this.bling.request(config);
+  }
+};
